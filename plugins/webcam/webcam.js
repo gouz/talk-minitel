2f2431a6f12c8be7661be317d652c6c1cf584bd0
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-01-04 18:35:56 +0100

    feat: :sparkles: interactions

--- plugins/webcam/webcam.js
+++ plugins/webcam/webcam.js
@@ -1,12 +1,32 @@
 const video = document.querySelector("#webcam");
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch((_) => {
-      console.log("Something went wrong!");
-    });
+async function getConnectedDevices(type) {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  return devices.filter((device) => device.kind === type);
 }
+
+// Open camera with at least minWidth and minHeight capabilities
+async function openCamera(cameraId, minWidth, minHeight) {
+  const constraints = {
+    audio: { echoCancellation: true },
+    video: {
+      deviceId: cameraId,
+      width: { min: minWidth },
+      height: { min: minHeight },
+    },
+  };
+
+  return await navigator.mediaDevices.getUserMedia(constraints);
+}
+
+const webcamize = async () => {
+  const cameras = await getConnectedDevices("videoinput");
+  console.log(cameras);
+  if (cameras && cameras.length > 0) {
+    // Open first available video camera with a resolution of 1280x720 pixels
+    const stream = await openCamera(cameras[1].deviceId, 1280, 720);
+    video.srcObject = stream;
+  }
+};
+
+webcamize();
