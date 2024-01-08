67519df7c3a5bae5091c5f9c0ac606651b42c5a2
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-01-08 08:58:43 +0100

    feat: :sparkles: add fun

--- plugins/webcam/webcam.js
+++ plugins/webcam/webcam.js
@@ -5,7 +5,6 @@ async function getConnectedDevices(type) {
   return devices.filter((device) => device.kind === type);
 }
 
-// Open camera with at least minWidth and minHeight capabilities
 async function openCamera(cameraId, minWidth, minHeight) {
   const constraints = {
     video: {
@@ -22,7 +21,7 @@ const webcamize = async () => {
   const cameras = await getConnectedDevices("videoinput");
   if (cameras && cameras.length > 0) {
     cameras.forEach((camera) => {
-      if (camera.label === "JOYACCESS JA-Webcam (0c45:89a0)") {
+      if (camera.label === window.slidesk.env.WEBCAM) {
         openCamera(camera.deviceId, 1280, 720).then(
           (stream) => (video.srcObject = stream)
         );
